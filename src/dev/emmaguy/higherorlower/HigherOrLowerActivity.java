85a6940c3cd83bf09e1c3d8560cc8b077b86524c
Emma Guy <emmag87@gmail.com>
2013-06-08 13:50:22 +0100

    initial commit

---
+++ src/dev/emmaguy/higherorlower/HigherOrLowerActivity.java
@@ -0,0 +1,15 @@
+package dev.emmaguy.higherorlower;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HigherOrLowerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+
+	setContentView(R.layout.activity_higher_or_lower);
+
+    }
+}
