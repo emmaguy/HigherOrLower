bd5a85b1b6206250d4164de6eb6e6dc033124176
Emma Guy <emmag87@gmail.com>
2013-06-16 13:03:00 +0100

    rolled back roundedcorners as wasn't working, tweaked splashs creen

--- src/dev/emmaguy/higherorlower/ui/RoundedCornersImageView.java
+++ src/dev/emmaguy/higherorlower/ui/RoundedCornersImageView.java
@@ -7,11 +7,11 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.Shader.TileMode;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
 public class RoundedCornersImageView extends ImageView {
-
     public RoundedCornersImageView(Context context) {
 	super(context);
     }
@@ -25,15 +25,34 @@ public class RoundedCornersImageView extends ImageView {
     }
 
     @Override
-    public void setImageBitmap(Bitmap bitmap) {
-	
-    	int width = getMeasuredWidth();
-    	int height = getMeasuredHeight();
+    protected void onDraw(Canvas canvas) {
+	BitmapDrawable drawable = (BitmapDrawable) getDrawable();
 
-	super.setImageBitmap(getRoundedCornerBitmap(getContext(), bitmap, 5, width, height));
+	if (drawable == null) {
+	    return;
+	}
+
+	if (getWidth() == 0 || getHeight() == 0) {
+	    return;
+	}
+
+	Bitmap fullSizeBitmap = drawable.getBitmap();
+
+	int scaledWidth = getMeasuredWidth();
+	int scaledHeight = getMeasuredHeight();
+
+	Bitmap scaledBitmap;
+	if (scaledWidth == fullSizeBitmap.getWidth() && scaledHeight == fullSizeBitmap.getHeight()) {
+	    scaledBitmap = fullSizeBitmap;
+	} else {
+	    scaledBitmap = Bitmap.createScaledBitmap(fullSizeBitmap, scaledWidth, scaledHeight, true);
+	}
+
+	Bitmap roundBitmap = getRoundedCornerBitmap(getContext(), scaledBitmap, 5, scaledWidth, scaledHeight);
+	canvas.drawBitmap(roundBitmap, 0, 0, null);
     }
-     
-    private Bitmap getRoundedCornerBitmap(Context context, Bitmap bitmap, int roundPixels, int width, int height) {
+
+    public Bitmap getRoundedCornerBitmap(Context context, Bitmap bitmap, int roundPixels, int width, int height) {
 
 	final float scaledRoundPixels = roundPixels * context.getResources().getDisplayMetrics().density;
 	Bitmap roundedBitmap = Bitmap.createBitmap(width, height, bitmap.getConfig());
@@ -41,7 +60,9 @@ public class RoundedCornersImageView extends ImageView {
 	Paint paint = new Paint();
 	paint.setAntiAlias(true);
 	paint.setShader(new BitmapShader(bitmap, TileMode.CLAMP, TileMode.CLAMP));
-	canvas.drawRoundRect((new RectF(0.0f, 0.0f, bitmap.getWidth(), bitmap.getHeight())), scaledRoundPixels, scaledRoundPixels, paint);
+	canvas.drawRoundRect((new RectF(0.0f, 0.0f, bitmap.getWidth(), bitmap.getHeight())), scaledRoundPixels,
+		scaledRoundPixels, paint);
 	return roundedBitmap;
     }
+
 }
